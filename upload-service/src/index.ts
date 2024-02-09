ba50d043a25078acbcbac6e235b6267086f38815
leoAshu <ashutosh.ojha2009@gmail.com>
2024-02-08 17:39:47 -0800

    implements cloning a git repository from git url

--- upload-service/src/index.ts
+++ upload-service/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import simpleGit from 'simple-git'
+
 import { cfg, generateUID } from './utils'
 
 const app = express()
@@ -7,11 +9,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.post('/deploy', (req, res) => {
+app.post('/deploy', async (req, res) => {
+    const uid = generateUID()
     const repoUrl = req.body.repoUrl
-    console.log(repoUrl)
 
-    res.status(200).json({})
+    await simpleGit().clone(repoUrl, `./output/${uid}`)
+
+    res.status(200).json({ uid })
 })
 
 app.listen(cfg.PORT)
