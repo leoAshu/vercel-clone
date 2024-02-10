f429619969cfe7256283c05e74464caabb012a45
leoAshu <ashutosh.ojha2009@gmail.com>
2024-02-10 13:49:51 -0800

    implements request handler service to serve the requested files specified by the subdomain of the url

--- request-handler/src/cfg.ts
+++ request-handler/src/cfg.ts
@@ -3,6 +3,12 @@ dotenv.config()
 
 const cfg = {
     PORT: process.env.PORT || 3001,
+
+    R2_ROOT_FOLDER: process.env.R2_ROOT_FOLDER || 'output',
+    R2_BUCKET: process.env.R2_BUCKET || '',
+    R2_ID: process.env.R2_ID,
+    R2_SECRET: process.env.R2_SECRET,
+    R2_ENDPOINT: process.env.R2_ENDPOINT,
 }
 
 export default cfg
--- request-handler/src/index.ts
+++ request-handler/src/index.ts
@@ -1,13 +1,37 @@
 import express from 'express'
+import { S3 } from 'aws-sdk'
 import cfg from './cfg'
 
 const app = express()
 
-app.get('/*', (req, res) => {
+const s3 = new S3({
+    accessKeyId: cfg.R2_ID,
+    secretAccessKey: cfg.R2_SECRET,
+    endpoint: cfg.R2_ENDPOINT,
+})
+
+app.get('/*', async (req, res) => {
     const host = req.hostname
+
     const id = host.split('.')[0]
 
-    const filePath = req.path
+    const filePath = req.path === '/' ? '/index.html' : req.path
+
+    const contents = await s3
+        .getObject({
+            Bucket: cfg.R2_BUCKET,
+            Key: `dist/${id}${filePath}`,
+        })
+        .promise()
+
+    const type = filePath.endsWith('html')
+        ? 'text/html'
+        : filePath.endsWith('css')
+        ? 'text/css'
+        : 'application/javascript'
+
+    res.set('Content-Type', type)
+    res.send(contents.Body)
 })
 
 app.listen(cfg.PORT)
